f91dd6a39b500905fff11106c9f38d431c28914b
Enkidu93 <lowryec17@gcc.edu>
2025-11-30 16:31:22 -0500

    Fix typo

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.title("Christmas exchange!")
 with st.form("NameForm"):
     name = st.text_input(label="Enter the full first name of the sibling whose family or group you're a part of...")
     if st.form_submit_button():
-        if name.capitalize() not in st.session_state["names"]:
+        if name.capitalize() not in st.session_state["doc"]["names"]:
             st.error("Unknown name")
         else:
             st.session_state["name"] = name.capitalize()
